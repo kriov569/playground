799395820bd39e8630d63711a93c528bc20e9553
kriov569 <nikita.veshnyakov2014@gmail.com>
2024-04-09 09:18:31 +0300

    Fixed top3

--- src/ResultsBoard.java
+++ src/ResultsBoard.java
@@ -4,24 +4,33 @@ import java.util.*;
 //a. Каждая запись содержит имя студента и его средний балл.
 //b. Реализовать структуру, которая хранит записи в порядке возрастания среднего балла.
 //c. Вывести имена 3-х самых успешных студентов в порядке убывания среднего балла.
+
 public class ResultsBoard {
-    private TreeMap<Float, String> studentMap;
+    private TreeMap<Float, List<String>> studentMap;
 
     public ResultsBoard() {
         studentMap = new TreeMap<>(Collections.reverseOrder());
     }
+
     void addStudent(String name, Float score) {
-        studentMap.put(score, name);
+        if (!studentMap.containsKey(score)) {
+            studentMap.put(score, new ArrayList<>());
+        }
+        studentMap.get(score).add(name);
     }
 
     List<String> top3() {
         List<String> top3Students = new ArrayList<>();
         int count = 0;
-        for (String name : studentMap.values()) {
-            top3Students.add(name);
-            count++;
-            if (count == 3)
-                break;
+        for (Map.Entry<Float, List<String>> entry : studentMap.entrySet()) {
+            List<String> names = entry.getValue();
+            for (String name : names) {
+                top3Students.add(name);
+                count++;
+                if (count == 3) {
+                    return top3Students;
+                }
+            }
         }
         return top3Students;
     }
@@ -42,3 +51,4 @@ public class ResultsBoard {
         }
     }
 }
+
